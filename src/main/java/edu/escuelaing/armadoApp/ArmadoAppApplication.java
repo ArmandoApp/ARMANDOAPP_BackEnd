3fe6eb749a97b574d2fa320b7980452fd780d010
Daniel porras <edward.porras@mail.escuelaing.edu.co>
2022-02-06 14:59:44 -0500

    inicio proyecto

---
+++ src/main/java/edu/escuelaing/armadoApp/ArmadoAppApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.armadoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmadoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmadoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/armadoApp/ArmadoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.armadoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmadoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
